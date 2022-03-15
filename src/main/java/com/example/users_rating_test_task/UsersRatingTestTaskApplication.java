b4e9c9afd72c78bb77c75bc08a7fcebd53e4565a
kpavlunenko <kpavlunenko@gmail.com>
2022-03-15 20:12:01 +0200

    test task

---
+++ src/main/java/com/example/users_rating_test_task/UsersRatingTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.users_rating_test_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersRatingTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsersRatingTestTaskApplication.class, args);
+    }
+
+}
